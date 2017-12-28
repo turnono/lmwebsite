9e76a95bf6e9e45d7b0454ffa875197f7f5930e5
turnono <turnono@gmail.om>
2017-12-28 09:40:58 +0200

    service worker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+
 
 $(document).ready(function(){
     var ua = navigator.userAgent;
